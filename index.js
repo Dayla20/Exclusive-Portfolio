34c4776972188aae618d18b38e334f50b1387e53
Dayla20 <shilly_riser@yahoo.com>
2025-11-27 19:15:23 -0600

    Portfolio6

---
+++ index.js
@@ -0,0 +1,21 @@
+// Integrating emailjs service
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");   
+    loading.classList += (" modal__overlay--visible"); 
+
+emailjs
+    .sendForm(
+        "service_zhtz31e",
+        "template_f3kdeey",
+        event.target,
+        "y9EbGYMVnU0P_s63i"
+    ).then(()=> {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += (" modal__overlay--visible");
+    }).catch(() => { 
+        loading.classList.remove("modal__overlay--visible");
+        alert("The email service is temporarily unavailable, please contact me at freitasdiana@gmail.com");
+    }) 
+}
